08e8aa25b57feb5769a2156dab9ccbe3aa0a02cf
Pragya Mukherjee <Pragyamukherjee2017@gmail.com>
2023-05-22 22:17:21 +0530

    fix: changed UI labels
    Added descriptive labels to UI inputs
    Also added docstring to predict.py file
    
    Signed off by Pragya

--- app.py
+++ app.py
@@ -5,22 +5,21 @@ import numpy as np
 st.title('Dementia prediction')
  
 st.write('---')
- 
+
 
  
-st.markdown("**Please enter the details in the form**")
+st.markdown("**Please enter the correct details from the MRI report - **")
 
-MR_Delay=st.number_input('Enter MR_Delay')
-gender = st.sidebar.selectbox('gender',(0,1))
-#area = st.slider('Area of the house', 1000, 5000, 1500)
+MR_Delay=st.number_input('Enter MR Delay')
+gender = st.sidebar.selectbox('Gender (select 0 for male and 1 for female)',(0,1))
 age = st.number_input('Age (in years)?')
-EDUC=  st.number_input('Enter EDUC')
-SES= st.number_input('Enter SES')
-MMSE=  st.number_input('Enter MMS')
-CDR=  st.number_input('Enter CDR')
-eTIV=  st.number_input('Enter eTIV')
-nWBV= st.number_input('Enter nWBV')
-ASF=st.number_input('Enter ASF')
+EDUC=  st.number_input('Enter years of education')
+SES= st.number_input('Enter Socio-economic Status (1-5)')
+MMSE=  st.number_input('Enter Mini Mental State Examination (MMSE)')
+CDR=  st.number_input('Enter Clinical Dementia Rating (CDR)')
+eTIV=  st.number_input('Enter estimated total intracranial volume (eTIV)')
+nWBV= st.number_input('Enter Normalize Whole Brain Volume (nWBV)')
+ASF=st.number_input('Enter Atlas Scaling Factor (ASF)')
  
 if st.button('Predict'):
     my_pred = predict(np.array([[(MR_Delay),	gender,     age,	EDUC,	SES,	MMSE,	CDR,	eTIV,	nWBV,	ASF]]))
--- predict.py
+++ predict.py
@@ -1,5 +1,19 @@
 import joblib
  
 def predict(data):
+    """Loads the model and predicts the class of the instance
+
+    Parameters
+    ----------
+    data : array
+        input data from UI
+
+    Returns
+    -------
+    list[int]
+        returns class - 0 for Demented and 1 for Non-demented
+    """
+    
+    
     nb = joblib.load('nb_model.sav')
     return nb.predict(data) 
